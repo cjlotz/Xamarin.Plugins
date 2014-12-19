6c92dc042ff6af25b2c17ff55a306d504a67b8e0
Carel Lotz <carel.lotz@gmail.com>
2014-12-19 07:50:43 +0200

    added nuspec file and globalassemblyinfo

---
+++ Messaging/Common/GlobalAssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+#if DEBUG
+[assembly: AssemblyConfiguration("DEBUG")]
+#endif
+#if RELEASE
+[assembly: AssemblyConfiguration("RELEASE")]
+#endif
+
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lotz.Xam.Messaging")]
+[assembly: AssemblyCopyright("Copyright © Carel Lotz 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
