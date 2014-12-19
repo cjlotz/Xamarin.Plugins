678258e5d29fdec2de30b93bc0bcb593a09617a5
Carel Lotz <carel.lotz@gmail.com>
2014-12-19 07:05:57 +0200

    rename messaging.ios to messaging.iosunified

---
+++ Messaging/Lotz.Xam.Messaging.iOSUnified/MessagingExtensions.cs
@@ -0,0 +1,25 @@
+using System;
+using Lotz.Xam.Messaging.Abstractions;
+
+namespace Lotz.Xam.Messaging
+{
+    public static class MessagingExtensions
+    {
+        #region Methods
+
+        public static MessagingContext AsPlatformContext(this IMessagingContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException("context");
+
+            var iOSContext = context as MessagingContext;
+
+            if (iOSContext == null)
+                throw new ArgumentException("Context not of type iOS MessagingContext", "context");
+
+            return iOSContext;
+        }
+
+        #endregion
+    }
+}
---
+++ Messaging/Lotz.Xam.Messaging.iOSUnified/SmsTask.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Threading.Tasks;
+using Lotz.Xam.Messaging.Abstractions;
+using MessageUI;
+using UIKit;
+
+namespace Lotz.Xam.Messaging
+{
+    public class SmsTask : ISmsTask
+    {
+        private MessagingContext _context;
+        private MFMessageComposeViewController _smsController;
+
+        public SmsTask(IMessagingContext context)
+        {
+            _context = context.AsPlatformContext();
+        }
+
+        #region ISmsTask Members
+
+        public Task SendSmsAsync(SmsMessageRequest sms)
+        {
+            var tcs = new TaskCompletionSource<object>();
+
+            if (MFMessageComposeViewController.CanSendText)
+            {
+                _smsController = new MFMessageComposeViewController();
+
+                _smsController.Recipients = new[] { sms.DestinationAddress };
+                _smsController.Body = sms.Message;
+
+                EventHandler<MFMessageComposeResultEventArgs> handler = null;
+                handler = (sender, args) =>
+                {
+                    _smsController.Finished -= handler;
+
+                    var uiViewController = sender as UIViewController;
+                    if (uiViewController == null)
+                    {
+                        throw new ArgumentException("sender");
+                    }
+
+                    uiViewController.DismissViewController(true, () => { });
+                    tcs.SetResult(null);
+                };
+
+                _smsController.Finished += handler;
+
+                _context.ViewController.PresentViewController(_smsController, true, () => {});
+            }
+            else
+            {
+                tcs.SetResult(null);
+            }
+
+            return tcs.Task;
+        }
+
+        #endregion
+    }
+}
