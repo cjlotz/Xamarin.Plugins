4a923a2359209caf2235f11fdad5a2d154aebd26
James Montemagno <james.montemagno@gmail.com>
2015-12-06 19:18:00 -0700

    Add UWP support, bump to 2.4.0

--- Messaging/Common/Build.cmd
+++ Messaging/Common/Build.cmd
@@ -1,1 +1,1 @@
-msbuild ..\..\build.proj /p:MajorVersion=2;MinorVersion=3;Build=0;Revision=0;Prerelease= /v:minimal
+msbuild ..\..\build.proj /p:MajorVersion=2;MinorVersion=4;Build=0;Revision=0;Prerelease= /v:minimal
